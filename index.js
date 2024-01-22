62b5f1118c82cb6f42cd6bb9ffce2d0deb2f7270
Jithin C <jith97972@gmail.com>
2024-01-22 07:41:24 +0530

    gecoder changes

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const corsOptions  = {
 }
 dotenv.config()
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://127.0.0.1:5173','https://homeonrentv.onrender.com'],
+    origin: ['http://localhost:5173', 'http://127.0.0.1:5173',],
     methods: ['GET', 'PUT', 'PATCH', 'POST', 'DELETE'],
     credentials: true
 }));    app.use(cookieParser());
--- src/models/property.js
+++ src/models/property.js
@@ -1,102 +1,120 @@
-import geocoder from '../utils/geocoder.js'
-import mongoose from "mongoose"
-
-const PropertySchema=new mongoose.Schema({
+import geocoder from "../utils/geocoder.js";
+import mongoose from "mongoose";
+import opencage from "opencage-api-client";
+const PropertySchema = new mongoose.Schema(
+  {
     location: {
-        type: {
-          type: String,
-          enum: ['Point']
-        },
-        coordinates: {
-          type: [Number],
-          index: '2dsphere'
-        },
-        formattedAddress: String,
-        city:String,
-        streetName:String
+      type: {
+        type: String,
+        enum: ["Point"],
       },
-title:{
-        type:String,
-        required:true,
-        unique:true
+      coordinates: {
+        type: [Number],
+        index: "2dsphere",
+      },
+      formattedAddress: String,
+      city: String,
+      streetName: String,
     },
-    type:{
-        type:String,
-        required:true,
-        enum:["4BHK","3BHK","2BHK"]
+    title: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    desc:{
-        type:String,
-        required:true,
-        min:6
+    type: {
+      type: String,
+      required: true,
+      enum: ["4BHK", "3BHK", "2BHK"],
     },
-    img:{
-        type:[],
-        default:""
+    desc: {
+      type: String,
+      required: true,
+      min: 6,
+    },
+    img: {
+      type: [],
+      default: "",
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    sqrmeters: {
+      type: Number,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    featured: {
+      type: Boolean,
+      default: false,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: Number,
+      required: true,
+    },
+    name: {
+      type: String,
+    },
+    status: {
+      type: String,
 
+      default: "pending",
     },
-    price:{
-        type:Number,
-        required:true
-    },
-    sqrmeters:{
-        type:Number,
-        required:true,
-       
-    },
-    address:{
-        type:String,
-        required:true,
-    },
-    featured:{
-        type:Boolean,
-        default:false
-    },
-    email:{
-        type:String,
-        required:true,
-    },
-    phone:{
-        type:Number,
-        required:true
-    },
-    name:{
-        type:String,
-       
-    },status:{
-        type:String,
-       
-        default:"pending"
-    },furnishment:{
-        type:String,
-       
+    furnishment: {
+      type: String,
     },
     schedules: {
-        type: [],
-        default: []
-      },
-      ownerDetailsViewed: {
-        type: [],
-        default: []
-      },
-      enqueryDetails: {
-        type: [],
-        default: []
-      },location:{
-        type:String
-      }
-      
-},{timestamps:true})
-PropertySchema.pre('save',async function(next){
-    const loc=await geocoder.geocode(this.address)
-    this.location={
-        type: 'Point',
-        coordinates: [loc[0].longitude, loc[0].latitude],
-        formattedAddress: loc[0].formattedAddress,
-        city:loc[0].city,
-        streetName:loc[0].streetName
-    }
-})
+      type: [],
+      default: [],
+    },
+    ownerDetailsViewed: {
+      type: [],
+      default: [],
+    },
+    enqueryDetails: {
+      type: [],
+      default: [],
+    },
+   
+  },
+  { timestamps: true }
+);
+PropertySchema.pre("save", async function (next) {
+  try {
+     const data = await opencage.geocode({ q: "Theresienhöhe 11, München" });
+
+     if (data.status.code === 200 && data.results.length > 0) {
+        const place = data.results[0];
+        this.location = {
+           type: "Point",
+           coordinates: [place.geometry.lat, place.geometry.lng],
+           formattedAddress: place.formatted,
+           city: place.components.city,
+           streetName: place.components.quarter,
+        };
+     } else {
+        console.log("Status", data.status.message);
+        console.log("total_results", data.total_results);
+     }
+
+     next();
+  } catch (error) {
+     console.log("Error", error.message);
+
+     if (error.status.code === 402) {
+        console.log("hit free trial daily limit");
+     }
+
+     next(error);
+  }
+});
 
 
-export default mongoose.model("property",PropertySchema)
+export default mongoose.model("property", PropertySchema);
